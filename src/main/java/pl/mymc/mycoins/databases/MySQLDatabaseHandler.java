7247ae5de2d8de5916727965b5b3ac7766adcef6
WieszczY <wieszczy85@gmail.com>
2024-06-30 21:33:42 +0200

    Przeniesiono część logów do debugMode + Dodano obsługe DailyReward

--- src/main/java/pl/mymc/mycoins/databases/MySQLDatabaseHandler.java
+++ src/main/java/pl/mymc/mycoins/databases/MySQLDatabaseHandler.java
@@ -12,14 +12,19 @@ public class MySQLDatabaseHandler {
     private final String host, database, username, password;
     private final int port;
     private final MyCoinsLogger logger;
+    private final FileConfiguration config;
+    private final boolean debugMode;
 
     public MySQLDatabaseHandler(FileConfiguration config, MyCoinsLogger logger) {
+        this.config = config;
+        this.debugMode = config.getBoolean("debug");
         this.host = config.getString("database.mysql.host");
         this.port = config.getInt("database.mysql.port");
         this.database = config.getString("database.mysql.name");
         this.username = config.getString("database.mysql.username");
         this.password = config.getString("database.mysql.password");
         this.logger = logger;
+
     }
 
     public void openConnection() throws SQLException, ClassNotFoundException {
@@ -40,9 +45,11 @@ public class MySQLDatabaseHandler {
         return connection;
     }
 
-    public void createTable() throws SQLException {
+    public void createTable() {
         try (Statement statement = getConnection().createStatement()) {
-            logger.info("Sprawdzanie tabeli My-Coins w bazie danych");
+            if(debugMode) {
+                logger.info("Sprawdzanie tabeli My-Coins w bazie danych");
+            }
             String createTable = "CREATE TABLE IF NOT EXISTS `My-Coins` (" +
                     "`id` int(11) NOT NULL AUTO_INCREMENT," +
                     "`player` varchar(255) NOT NULL," +
@@ -55,11 +62,28 @@ public class MySQLDatabaseHandler {
                     ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
 
             statement.execute(createTable);
-            logger.info("Zakończono operacje na tabeli");
+            if(debugMode) {
+                logger.info("Zakończono operacje na tabeli My-Coins");
+            }
+            if(debugMode) {
+                logger.info("Sprawdzanie tabeli DailyRewards w bazie danych");
+            }
+            String createDailyRewardsTable = "CREATE TABLE IF NOT EXISTS `DailyRewards` (" +
+                    "`uuid` varchar(255) NOT NULL," +
+                    "`date` date NOT NULL DEFAULT curdate()," +
+                    "`remainingReward` double NOT NULL," +
+                    "PRIMARY KEY (`uuid`, `date`)" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+
+            statement.execute(createDailyRewardsTable);
+            if(debugMode) {
+                logger.info("Zakończono operacje na tabeli DailyRewards");
+            }
         }catch (SQLException e) {
             logger.err("Nie udało się wykonać operacji na bazie danych!" + e.getMessage());
         }
     }
+
     public void openConnectionAndCreateTable() throws SQLException, ClassNotFoundException {
         try {
             openConnection();
@@ -114,6 +138,47 @@ public class MySQLDatabaseHandler {
             }
         }
         return 0;
+    }
+
+    public double getPlayerDailyReward(String playerUUID) throws SQLException {
+        try (PreparedStatement statement = getConnection().prepareStatement("SELECT remainingReward FROM `DailyRewards` WHERE uuid = ? AND date = ?;")) {
+            statement.setString(1, playerUUID);
+            statement.setDate(2, java.sql.Date.valueOf(LocalDate.now().toString()));
+            try (ResultSet rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getDouble("remainingReward");
+                } else {
+                    return 0;
+                }
+            }catch (SQLException e) {
+                logger.err("Nie udało się pobrać dziennego limitu nagród z bazy danych. Szczegóły błędu: " + e.getMessage());
+            }
+        }
+        return 0;
+    }
+
+    public void addNewDailyRewardEntry(String playerUUID, double dailyLimit) {
+        try (PreparedStatement statement = getConnection().prepareStatement("INSERT INTO `DailyRewards` (uuid, date, remainingReward) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE remainingReward = ?;")) {
+            statement.setString(1, playerUUID);
+            statement.setDate(2, java.sql.Date.valueOf(LocalDate.now().toString()));
+            statement.setDouble(3, dailyLimit);
+            statement.setDouble(4, dailyLimit);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            logger.err("Nie udało się dodać nowego wpisu do tabeli DailyRewards. Szczegóły błędu: " + e.getMessage());
+        }
+    }
+
+    public void updateRemainingReward(String playerUUID, double reward) {
+        try (PreparedStatement statement = getConnection().prepareStatement("INSERT INTO `DailyRewards` (uuid, date, remainingReward) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE remainingReward = remainingReward - ?;")) {
+            statement.setString(1, playerUUID);
+            statement.setDate(2, java.sql.Date.valueOf(LocalDate.now().toString()));
+            statement.setDouble(3, config.getDouble("reward.daily_limit"));
+            statement.setDouble(4, reward);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            logger.err("Nie udało się zaktualizować pozostałej nagrody w bazie danych. Szczegóły błędu: " + e.getMessage());
+        }
     }
 
     public void closeConnection() {
