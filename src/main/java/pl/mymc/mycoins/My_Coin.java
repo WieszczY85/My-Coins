4ca5e8e8185d8ca7c510f04e75f49f0e1f856c92
WieszczY <wieszczy85@gmail.com>
2024-06-30 21:35:29 +0200

    Dodać obsługę "daily limit" + aktualizacje od MyCoinsMessages

--- src/main/java/pl/mymc/mycoins/My_Coin.java
+++ src/main/java/pl/mymc/mycoins/My_Coin.java
@@ -1,30 +1,33 @@
 package pl.mymc.mycoins;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.configuration.file.FileConfiguration;
+
 import pl.mymc.mycoins.databases.MySQLDatabaseHandler;
 import pl.mymc.mycoins.myc.PlayerTimeTracker;
 import pl.mymc.mycoins.helpers.MyCoinsDependencyManager;
 import pl.mymc.mycoins.helpers.MyCoinsLogger;
 import pl.mymc.mycoins.helpers.MyCoinsVersionChecker;
+
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.RegisteredServiceProvider;
-import org.bukkit.configuration.file.FileConfiguration;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URLClassLoader;
 import java.sql.SQLException;
-import java.time.Duration;
 
-import static org.codehaus.plexus.util.FileUtils.extension;
 
 public final class My_Coin extends JavaPlugin {
 
     //TODO:
     // * Debug - przenieść część instniejących logów (zmniejszenie spamu w konsoli)
     // * Dodać multiplikacje dla rang + weryfikacja rang z Vault
-    // * Dodać obsługę "daily limit"
     // * Dodać sprawdzanie czy gracz się porusza i jeśli nie to czy ma AFK
     // * Dodać obsługe AFK z EternalsCore i EssentialsX
 
@@ -33,26 +36,36 @@ public final class My_Coin extends JavaPlugin {
     private static Permission perms = null;
     private MySQLDatabaseHandler dbHandler;
     private PlayerTimeTracker timeTracker;
-    private FileConfiguration config;
+    private final FileConfiguration config;
+    private final boolean debugMode;
+    private final boolean checkForUpdates;
+    private final boolean autoDownloadUpdates;
+    private FileConfiguration localConfig;
 
     public My_Coin() {
         String fullName = getDescription().getFullName();
         String pluginName = getDescription().getRawName();
         String serverVersion = getServer().getBukkitVersion();
-        boolean debugMode = config.getBoolean("debug");
+        this.config = getConfig();
+        this.debugMode = config.getBoolean("debug");
+        this.checkForUpdates = config.getBoolean("checkForUpdates");
+        this.autoDownloadUpdates = config.getBoolean("autoDownloadUpdates");
         this.logger = new MyCoinsLogger(fullName, serverVersion, pluginName, debugMode);
     }
 
     @Override
     public void onLoad() {
         saveDefaultConfig();
+        createLocalConfig();
         MyCoinsDependencyManager dm = new MyCoinsDependencyManager((URLClassLoader) getClass().getClassLoader(), logger);
         try {
             dm.loadMariaDb();
             dm.loadAdventureApi();
             dm.loadLegacy();
             dm.loadMiniMessage();
-            logger.success("Zakończono ładowanie wszystkie zależności!");
+            if (debugMode) {
+                logger.success("Zakończono ładowanie wszystkie zależności!");
+            }
         } catch (Exception e) {
             logger.err("Wystąpił błąd podczas ładowania zależności: " + e.getMessage());
         }
@@ -62,10 +75,6 @@ public final class My_Coin extends JavaPlugin {
     public void onEnable() {
         String pluginName = getDescription().getRawName();
         String currentVersion = getDescription().getVersion();
-
-        FileConfiguration config = getConfig();
-        boolean checkForUpdates = config.getBoolean("checkForUpdates");
-        boolean autoDownloadUpdates = config.getBoolean("autoDownloadUpdates");
 
         if (!setupService(Economy.class)) {
             logger.err("Wyłączono, ponieważ nie znaleziono zależności Vault");
@@ -81,11 +90,27 @@ public final class My_Coin extends JavaPlugin {
             throw new RuntimeException(e);
         }
 
-        timeTracker = new PlayerTimeTracker(dbHandler, logger, config);
+        timeTracker = new PlayerTimeTracker(dbHandler, logger, config, localConfig);
         getServer().getPluginManager().registerEvents(timeTracker, this);
         MyCoinsVersionChecker.checkVersion(pluginName, currentVersion, logger, checkForUpdates, autoDownloadUpdates);
         logger.pluginStart();
     }
+
+    private void createLocalConfig() {
+        File localFile = new File(getDataFolder(), "local.yml");
+        if (!localFile.exists()) {
+            localFile.getParentFile().mkdirs();
+            saveResource("local.yml", false);
+        }
+
+        localConfig = new YamlConfiguration();
+        try {
+            localConfig.load(localFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            logger.err("Wystąpił błąd podczas ładowania pliku local.yml: " + e.getMessage());
+        }
+    }
+
     private boolean setupService(Class<?> serviceClass) {
         RegisteredServiceProvider<?> rsp = getServer().getServicesManager().getRegistration(serviceClass);
         if (rsp == null) {
