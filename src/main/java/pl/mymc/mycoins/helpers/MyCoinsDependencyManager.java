70cea368627f5fce72eb9ad99090383b1a30d6ed
WieszczY <wieszczy85@gmail.com>
2024-07-01 22:43:23 +0200

    Przebudowa systemu ładowania zależności

--- src/main/java/pl/mymc/mycoins/helpers/MyCoinsDependencyManager.java
+++ src/main/java/pl/mymc/mycoins/helpers/MyCoinsDependencyManager.java
@@ -1,83 +1,100 @@
 package pl.mymc.mycoins.helpers;
 
-import java.io.InputStream;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.eclipse.aether.*;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
+import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.repository.LocalRepository;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
+import org.eclipse.aether.spi.connector.transport.TransporterFactory;
+import org.eclipse.aether.transport.file.FileTransporterFactory;
+import org.eclipse.aether.transport.http.HttpTransporterFactory;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.security.MessageDigest;
 
 public class MyCoinsDependencyManager {
-    private final URLClassLoader classLoader;
     private final MyCoinsLogger logger;
+    private final RepositorySystem system;
+    private final RepositorySystemSession session;
 
-    public MyCoinsDependencyManager(URLClassLoader classLoader, MyCoinsLogger logger) {
-        this.classLoader = classLoader;
+    public MyCoinsDependencyManager(MyCoinsLogger logger) {
         this.logger = logger;
+        this.system = newRepositorySystem();
+        this.session = newSession(system);
     }
 
-    private String calculateChecksum(Path path) throws Exception {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] fileBytes = Files.readAllBytes(path);
-        byte[] checksumBytes = digest.digest(fileBytes);
-        StringBuilder sb = new StringBuilder();
-        for (byte b : checksumBytes) {
-            sb.append(String.format("%02x", b));
-        }
-        return sb.toString();
+    public void loadDependency(String coordinates, Path targetPath) throws Exception {
+        Artifact artifact = new DefaultArtifact(coordinates);
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.setArtifact(artifact);
+        artifactRequest.addRepository(new RemoteRepository.Builder("central", "default", "https://repo1.maven.org/maven2/").build());
+
+        ArtifactResult artifactResult = system.resolveArtifact(session, artifactRequest);
+        artifact = artifactResult.getArtifact();
+
+        Files.copy(artifact.getFile().toPath(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+        logger.success("Pobrano zależność z " + coordinates);
     }
 
-    public void loadDependency(String url, Path targetPath) throws Exception {
-        // Utwórz katalog, jeśli nie istnieje
-        Files.createDirectories(targetPath.getParent());
+    private RepositorySystem newRepositorySystem() {
+        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
+        locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
+        locator.addService(TransporterFactory.class, FileTransporterFactory.class);
+        locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
 
-        if (Files.exists(targetPath)) {
-            logger.success("Plik już istnieje, nie ma konieczności pobierania: " + targetPath);
-            return;
-        }
-        // Pobierz zależność
-        try (InputStream in = new URL(url).openStream()) {
-            Files.copy(in, targetPath, StandardCopyOption.REPLACE_EXISTING);
-        }
-        // Załaduj zależność
-        Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-        method.setAccessible(true);
-        method.invoke(classLoader, targetPath.toUri().toURL());
-
-        logger.success("Pobrano zależność z " + url);
+        return locator.getService(RepositorySystem.class);
     }
 
+    private RepositorySystemSession newSession(RepositorySystem system) {
+        DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+        LocalRepository localRepo = new LocalRepository("target/local-repo");
+        session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo));
+
+        return session;
+    }
 
     public void loadMariaDb() throws Exception {
-        loadDependency("https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/3.4.0/mariadb-java-client-3.4.0.jar", Paths.get("plugins/My-Coins/libs/mariadb-java-client-3.4.0.jar"));
+        loadDependency("org.mariadb.jdbc:mariadb-java-client:3.4.0", Paths.get("plugins/My-Coins/libs/mariadb-java-client-3.4.0.jar"));
         logger.success("Pomyślnie załadowano MariaDB!");
     }
+
     public void loadMysql() throws Exception {
-        loadDependency("https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.23/mysql-connector-java-8.0.23.jar", Paths.get("plugins/My-Coins/libs/mysql-connector-java-8.0.23.jar"));
+        loadDependency("mysql:mysql-connector-java:8.0.23", Paths.get("plugins/My-Coins/libs/mysql-connector-java-8.0.23.jar"));
         logger.success("Pomyślnie załadowano MySQL-Connector!");
     }
+
     public void loadMiniMessage() throws Exception {
-        loadDependency("https://repo1.maven.org/maven2/net/kyori/adventure-text-minimessage/4.17.0/adventure-text-minimessage-4.17.0.jar", Paths.get("plugins/My-Coins/libs/adventure-text-minimessage-4.17.0.jar"));
+        loadDependency("net.kyori:adventure-text-minimessage:4.17.0", Paths.get("plugins/My-Coins/libs/adventure-text-minimessage-4.17.0.jar"));
         logger.success("Pomyślnie załadowano MiniMessage!");
     }
+
     public void loadAdventureApi() throws Exception {
-        loadDependency("https://repo1.maven.org/maven2/net/kyori/adventure-api/4.17.0/adventure-api-4.17.0.jar", Paths.get("plugins/My-Coins/libs/adventure-api-4.17.0.jar"));
+        loadDependency("net.kyori:adventure-api:4.17.0", Paths.get("plugins/My-Coins/libs/adventure-api-4.17.0.jar"));
         logger.success("Pomyślnie załadowano AdventureAPI!");
     }
+
     public void loadGSON() throws Exception {
-        loadDependency("https://repo1.maven.org/maven2/net/kyori/adventure-text-serializer-gson/4.17.0/adventure-text-serializer-gson-4.17.0.jar", Paths.get("plugins/My-Coins/libs/adventure-text-serializer-gson-4.17.0.jar"));
+        loadDependency("net.kyori:adventure-text-serializer-gson:4.17.0", Paths.get("plugins/My-Coins/libs/adventure-text-serializer-gson-4.17.0.jar"));
         logger.success("Pomyślnie załadowano Adventure-GSON!");
     }
+
     public void loadLegacy() throws Exception {
-        loadDependency("https://repo1.maven.org/maven2/net/kyori/adventure-text-serializer-legacy/4.17.0/adventure-text-serializer-legacy-4.17.0.jar", Paths.get("plugins/My-Coins/libs/adventure-text-serializer-legacy-4.17.0.jar"));
+        loadDependency("net.kyori:adventure-text-serializer-legacy:4.17.0", Paths.get("plugins/My-Coins/libs/adventure-text-serializer-legacy-4.17.0.jar"));
         logger.success("Pomyślnie załadowano Adventure-Legacy!");
     }
+
     public void loadPlain() throws Exception {
-        loadDependency("https://repo1.maven.org/maven2/net/kyori/adventure-text-serializer-plain/4.17.0/adventure-text-serializer-plain-4.17.0.jar", Paths.get("plugins/My-Coins/libs/adventure-text-serializer-plain-4.17.0.jar"));
+        loadDependency("net.kyori:adventure-text-serializer-plain:4.17.0", Paths.get("plugins/My-Coins/libs/adventure-text-serializer-plain-4.17.0.jar"));
         logger.success("Pomyślnie załadowano Adventure-Plain!");
     }
+
 }
 
